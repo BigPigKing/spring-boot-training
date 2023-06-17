e88ad039368d0782b58a1e94c887c82ab15770c4
BigPigKingJames <ericzx103703012@gmail.com>
2023-06-17 09:30:56 +0800

    [Chore](none) Project initialization

---
+++ src/test/java/cht/eric/EricApplicationTests.java
@@ -0,0 +1,13 @@
+package cht.eric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
